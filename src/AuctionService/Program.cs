cad4d2b20298c2264f9a1acefbffc97fb825b843
sstoxen <sstoxen@hotmail.com>
2024-12-09 13:05:36 -0600

    Adding rabbitmq and contacts to auction service.

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,4 +1,5 @@
 using AuctionService.Data;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +13,13 @@ builder.Services.AddDbContext<AuctionDbContext>(options =>
 
 });
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context, config) =>
+    {
+        config.ConfigureEndpoints(context); ;
+    });
+});
 
 var app = builder.Build();
 
